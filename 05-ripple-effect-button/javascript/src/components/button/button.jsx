2b63b817e054f39369058d5e0224f076bc2d1d49
S1mon009 <skaszy25@gmail.com>
2023-08-28 15:52:53 +0200

    Add files via upload

---
+++ 05-ripple-effect-button/javascript/src/components/button/button.jsx
@@ -0,0 +1,8 @@
+import RippleBtn from "react-ripple-button";
+import "./button.scss";
+
+const Button = () => {
+  return <RippleBtn btnClassName="button" text="Click me" btn />;
+};
+
+export default Button;
