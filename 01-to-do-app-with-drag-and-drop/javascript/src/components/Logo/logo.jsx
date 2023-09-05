6b31b86fd77b733b3c8684b45a60aa243600df20
S1mon009 <105738321+S1mon009@users.noreply.github.com>
2023-09-05 12:30:31 +0200

    Delete 01-to-do-app-with-drag-and-drop directory

--- 01-to-do-app-with-drag-and-drop/javascript/src/components/Logo/logo.jsx
+++
@@ -1,8 +0,0 @@
-import "./logo.scss";
-import DarkLogo from "../../images/logo-black.svg";
-
-const Logo = () => {
-  return <img src={DarkLogo} alt="Logo" className="logo" id="dark-logo" />;
-};
-
-export default Logo;
